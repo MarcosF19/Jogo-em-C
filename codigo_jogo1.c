2f4798df7bba5b06212413d4a62c734aa182f377
MarcosF19 <marcosfelipek10@gmail.com>
2021-03-31 11:15:25 -0300

    correções

--- codigo_jogo1.c
+++ codigo_jogo1.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>  //abs()  
-#include <time.h> //time() e srand()
-#include <math.h>
+#include <stdlib.h> //abs()
+#include <time.h>   //time() e srand()
 
 #define NUMERO_DE_TENTATIVAS 3 //Define, constante e números mágicos
 
-int main(void)
+int main()
 {
   printf("************************************\n");
   printf("* Bem-vindo ao Jogo de Adivinhação *\n");
   printf("************************************\n");
 
-  int numerosecreto = 42;
   int chute;
+  int acertou = 0;
   int tentativas = 1;
   double pontos = 1000;
-  int outravariavel = pontos * 7;
-  double pontoperdidos; 
 
-  /*pontos = pontos + 2;
-  pontos = pontos * 4 / 3; 
-  pontos = pontos - 1; */
+  //função time e srand
+  srand(time(0));
+  int numerosecreto = rand() % 100;
+  //máquina só poderá chutar números de 0 a 99
+  //função rand - gerar número aletório
 
-  while (1)
+  while (!acertou)
   { //Não sabe a quantidade? while
 
     printf("Qual é o seu %do. chute? \n", tentativas);
@@ -33,9 +32,10 @@ int main(void)
       printf("Você não pode chutar números negativos\n");
       continue;
     }
+
     printf("Seu %do. chute foi %d\n", tentativas, chute);
 
-    int acertou = chute == numerosecreto;
+    acertou = chute == numerosecreto;
     int maior = chute > numerosecreto;
 
     if (acertou)
@@ -56,29 +56,12 @@ int main(void)
     }
 
     tentativas++;
-  }
-  //double -> "%f" || int -> "%d" 
-  for (int pontos = 1; pontos <= NUMERO_DE_TENTATIVAS; pontos++)
-  {
-    int pontos = ((4 * 2) + (1 / 2)) * 3 - 1; //função matemática abs()
-    double pontosperdidos = abs(chute - numerosecreto) / 2.0;
-    /*double pontosperdidos = (double)(chute - numerosecreto) / (double)2;*/
-    if(pontosperdidos <0){
-      pontosperdidos = pontosperdidos * -1; 
-    }
-  }
-  //função time e srand 
-  int segundos = time(0);
-  srand(segundos); 
 
-  //função rand - gerar número aletório 
-  int numerogrande = rand(); 
-  //máquina só poderá chutar números de 0 a 99.
-  //Operador % resto da divisão por 100 
-  int numerossecreto = numerogrande % 100; 
+    double pontosperdidos = abs(chute - numerosecreto) / 2.0;
+    pontos = pontos - pontosperdidos;
+  }
 
   printf("Você fez %.2f pontos\n", pontos);
-  printf("Fim de jogo!\n");
-  printf("Obrigado por jogar!", tentativas);
+  printf("Obrigado por jogar!\n", tentativas);
 }
 // página 67
