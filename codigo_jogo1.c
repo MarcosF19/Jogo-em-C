824a869281bd2e77e275c8575b393d2ce30bfef1
MarcosF19 <marcosfelipek10@gmail.com>
2021-04-01 23:33:25 -0300

    variavel i adicionada

--- codigo_jogo1.c
+++ codigo_jogo1.c
@@ -10,13 +10,20 @@ int main()
   printf("* Bem-vindo ao Jogo de Adivinhação *\n");
   printf("************************************\n");
 
+  int chute;
+  double pontos = 1000;
+
   int nivel;
   int totaldetentativas;
 
-  int chute;
   int acertou = 0;
-  int tentativas = 1;
-  double pontos = 1000;
+  //int tentativas = 1;
+
+  //função time e srand
+  srand(time(0));
+  int numerosecreto = rand() % 100;
+  //máquina só poderá chutar números de 0 a 99
+  //função rand - gerar número aletório
 
   printf("Qual o nível de dificuldades?\n");
   printf("(1) Fácil (2) Médio (3) Difícil\n\n");
@@ -36,17 +43,15 @@ int main()
     totaldetentativas = 6;
   }
 
-  //função time e srand
-  srand(time(0));
-  int numerosecreto = rand() % 100;
-  //máquina só poderá chutar números de 0 a 99
-  //função rand - gerar número aletório
+  //sabemos a quantidade de vezes usa o for
+  for (int i = 1; i <= totaldetentativas; i++)
+  { 
 
-  while (!acertou)
-  { //Não sabe a quantidade? while
+    printf("Tentativa %d de %a\n", i, totaldetentativas);
 
-    printf("Qual é o seu %do. chute? \n", tentativas);
+    printf("Qual é o seu %do. chute? \n", i);
     scanf("%d", &chute);
+    
 
     if (chute < 0)
     {
@@ -54,7 +59,7 @@ int main()
       continue;
     }
 
-    printf("Seu %do. chute foi %d\n", tentativas, chute);
+    printf("Seu %do. chute foi %d\n", i, chute);
 
     acertou = chute == numerosecreto;
     int maior = chute > numerosecreto;
@@ -76,13 +81,11 @@ int main()
       printf("Seu chute  foi menor do que o número secreto!\n");
     }
 
-    tentativas++;
-
     double pontosperdidos = abs(chute - numerosecreto) / 2.0;
     pontos = pontos - pontosperdidos;
   }
 
   printf("Você fez %.2f pontos\n", pontos);
-  printf("Obrigado por jogar!\n", tentativas);
+  printf("Obrigado por jogar!\n");
 }
-// página 71
+// página 72
